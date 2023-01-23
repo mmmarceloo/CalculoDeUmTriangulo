2248746746a56ee0b6ff691679d790169590a1df
Marcelo Moreira de Sousa <83249188+mmmarceloo@users.noreply.github.com>
2023-01-23 17:30:20 -0300

    Delete ConsultaCep directory

--- ConsultaCep/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsultaCep.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
